ae23d0dab20df97ac7c613edb4d5616eafcfa560
khadijahnabil <nabilkhadijah7@gmail.com>
2022-09-16 16:39:09 +0200

    display forecast data using API

--- src/script.js
+++ src/script.js
@@ -51,32 +51,53 @@ function updateTime() {
 
 updateTime(); //update time at page load
 
+/**format date of forecast */
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return day;
+}
+
 /**forecast HTML element */
 
 function displayForecast(response) {
-  console.log(response?.data?.daily);
+  let forecast = response?.data?.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-    <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-      <i class="fa-solid fa-cloud weather-icon"></i>
-      <div class="weather-forecast-temperature">
-        <span class="weather-forecast-temperature-min"> 28° </span>
-        <span class="weather-forecast-temperature-max"> 28° </span>
-      </div>
-    </div>
-  `;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 7 && index != 0) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="col-2">
+          <div class="weather-forecast-date">${formatDay(forecastDay?.dt)}</div>
+          <img 
+            src="http://openweathermap.org/img/wn/${
+              forecastDay?.weather[0]?.icon
+            }@2x.png"
+            alt=""
+            width= 50px; 
+          />
+          <div class="weather-forecast-temperature">
+            <span class="weather-forecast-temperature-min"> 
+            ${Math.round(forecastDay?.temp?.min)}°
+            </span>
+            <span>|</span>
+            <span class="weather-forecast-temperature-max"> 
+            ${Math.round(forecastDay?.temp?.max)}°
+            </span>
+          </div>
+        </div>
+        `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
-displayForecast(null); //update forecast HTML at page load
+// displayForecast(null); //update forecast HTML at page load
 
 /**get 8 days forecast at every search*/
 
