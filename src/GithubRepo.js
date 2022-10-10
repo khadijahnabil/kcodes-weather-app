5d164f3152655e69767cd1d3dc6d9870f70735b4
khadijahnabil <nabilkhadijah7@gmail.com>
2022-10-10 15:45:09 +0100

    edited "github repo link" to open in new tab when clicked

--- src/GithubRepo.js
+++ src/GithubRepo.js
@@ -6,7 +6,11 @@ export default function GithubRepo() {
   return (
     <div className="github-repo">
       <p>
-        <a href="https://github.com/khadijahnabil/weather-app-react">
+        <a
+          href="https://github.com/khadijahnabil/weather-app-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Open-source code{" "}
         </a>
         by Khadijah Nabil
